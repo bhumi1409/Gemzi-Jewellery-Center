695ba907107ca36e056df30b145d26875dd924e0
bhumi1409 <124908804+bhumi1409@users.noreply.github.com>
2025-07-25 15:02:01 +0530

    Add files via upload

---
+++ backend/GemziJewelleryCenterApplication.java
@@ -0,0 +1,11 @@
+package com.gemzi.jewellery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemziJewelleryCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GemziJewelleryCenterApplication.class, args);
+    }
+}
